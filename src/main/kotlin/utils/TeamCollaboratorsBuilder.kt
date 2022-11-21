5e81eaea480d73104ddf8f0463a8d02666878a2d
Alexandre Alves <alexandre.alves@ucsal.edu.br>
2022-11-21 20:15:44 -0300

    added specification files

--- src/main/kotlin/utils/TeamCollaboratorsBuilder.kt
+++ src/main/kotlin/utils/TeamCollaboratorsBuilder.kt
@@ -7,9 +7,24 @@ class TeamCollaboratorsBuilder {
         fun getTeamCollaborators(): List<Collaborator> {
             return listOf(
                 Collaborator(
-                    name = "Alexandre Karl Volkert Alves",
+                    name = "Alexandre Karl",
                     email = "alexandre.alves@ucsal.edu.br",
                     telephone = "71 99636-1186"
+                ),
+                Collaborator(
+                    name = "Antonio Carlos",
+                    email = "antoniocc.filho@ucsal.edu.br",
+                    telephone = "71 99401-1114"
+                ),
+                Collaborator(
+                    name = "Berman Schultz",
+                    email = "berman.schultz@ucsal.edu.br",
+                    telephone = "71 98772-3621"
+                ),
+                Collaborator(
+                    name = "Luiz Felipe Viana",
+                    email = "luizf.nascimento@ucsal.edu.br",
+                    telephone = "71 99671-4267"
                 )
             )
         }
